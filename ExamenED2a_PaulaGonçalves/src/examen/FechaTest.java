333cc231d75e7ef37e0df8388c1127e98d1a3928
PGMerino <smr13.2022j@gmail.com>
2024-03-13 17:35:18 +0100

    Proyecto sin documentar

---
+++ ExamenED2a_PaulaGonçalves/src/examen/FechaTest.java
@@ -0,0 +1,14 @@
+package examen;
+
+public class FechaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Fecha prueba1 = (0,12,2000);
+		new Fecha prueba2 = (20,0,2000);
+		new Fecha prueba3 = (20,12,0);
+		new Fecha prueba4 = (28,2,2000);
+		new Fecha prueba5 = (30,12,2000);
+	}
+
+}
